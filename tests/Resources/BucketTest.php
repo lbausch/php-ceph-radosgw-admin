d106a5c749de0cdfb9a2b795bc8a86550017b6e2
Lorenz Bausch <info@lorenzbausch.de>
2021-09-06 20:47:52 +0200

    Add more bucket tests

--- tests/Resources/BucketTest.php
+++ tests/Resources/BucketTest.php
@@ -39,8 +39,7 @@ final class BucketTest extends TestCase
         $request = $transactions[0]['request'];
 
         $this->assertEquals('GET', $request->getMethod());
-        $this->assertEquals('gateway', $request->getUri()->getHost());
-        $this->assertEquals('/admin/metadata/bucket', $request->getUri()->getPath());
+        $this->assertEquals('http://gateway/admin/metadata/bucket', $request->getUri());
     }
 
     /**
@@ -73,7 +72,139 @@ final class BucketTest extends TestCase
         $request = $transactions[0]['request'];
 
         $this->assertEquals('DELETE', $request->getMethod());
-        $this->assertEquals('/admin/bucket', $request->getUri()->getPath());
-        $this->assertEquals('bucket=mybucket', $request->getUri()->getQuery());
+        $this->assertEquals('http://gateway/admin/bucket?bucket=mybucket', $request->getUri());
+    }
+
+    /**
+     * @covers \LBausch\CephRadosgwAdmin\ApiRequest
+     * @covers \LBausch\CephRadosgwAdmin\ApiResponse
+     * @covers \LBausch\CephRadosgwAdmin\Client
+     * @covers \LBausch\CephRadosgwAdmin\Config
+     * @covers \LBausch\CephRadosgwAdmin\Middlewares\SignatureMiddleware
+     * @covers \LBausch\CephRadosgwAdmin\Resources\AbstractResource
+     * @covers \LBausch\CephRadosgwAdmin\Resources\Bucket::info
+     * @covers \LBausch\CephRadosgwAdmin\Signature\SignatureV4::signRequest
+     */
+    public function testBucketInfoIsRetrieved()
+    {
+        $transactions = [];
+
+        $config = $this->getConfigWithMockedHandlers($transactions, [
+            new Response(200, [], <<<'EOT'
+{
+    "bucket": "mybucket",
+    "num_shards": 1,
+    "tenant": "",
+    "bucket_quota": {
+        "enabled": false,
+        "check_on_raw": false,
+        "max_size": -1,
+        "max_size_kb": 0,
+        "max_objects": -1
+    }
+}
+EOT),
+        ]);
+
+        $client = Client::make('http://gateway', 'acesskey', 'secretkey', $config);
+
+        $response = $client->bucket()->info([
+            'bucket' => 'mybucket',
+        ]);
+
+        /** @var Request $request */
+        $request = $transactions[0]['request'];
+
+        $this->assertEquals('GET', $request->getMethod());
+        $this->assertEquals('http://gateway/admin/bucket?bucket=mybucket', $request->getUri());
+
+        $this->assertEquals([
+            'bucket' => 'mybucket',
+            'num_shards' => 1,
+            'tenant' => '',
+            'bucket_quota' => [
+                'enabled' => false,
+                'check_on_raw' => false,
+                'max_size' => -1,
+                'max_size_kb' => 0,
+                'max_objects' => -1,
+            ],
+        ], $response->get());
+    }
+
+    /**
+     * @covers \LBausch\CephRadosgwAdmin\ApiRequest
+     * @covers \LBausch\CephRadosgwAdmin\ApiResponse
+     * @covers \LBausch\CephRadosgwAdmin\Client
+     * @covers \LBausch\CephRadosgwAdmin\Config
+     * @covers \LBausch\CephRadosgwAdmin\Middlewares\SignatureMiddleware
+     * @covers \LBausch\CephRadosgwAdmin\Resources\AbstractResource
+     * @covers \LBausch\CephRadosgwAdmin\Resources\Bucket::check
+     * @covers \LBausch\CephRadosgwAdmin\Signature\SignatureV4::signRequest
+     */
+    public function testBucketIndexIsChecked()
+    {
+        $transactions = [];
+
+        $config = $this->getConfigWithMockedHandlers($transactions, [
+            new Response(200, [], '[]'),
+        ]);
+
+        $client = Client::make('http://gateway', 'acesskey', 'secretkey', $config);
+
+        $response = $client->bucket()->check('mybucket');
+
+        /** @var Request $request */
+        $request = $transactions[0]['request'];
+
+        $this->assertEquals('GET', $request->getMethod());
+        $this->assertEquals('http://gateway/admin/bucket?index=&bucket=mybucket', $request->getUri());
+
+        $this->assertEquals([], $response->get());
+    }
+
+    /**
+     * @covers \LBausch\CephRadosgwAdmin\ApiRequest
+     * @covers \LBausch\CephRadosgwAdmin\ApiResponse
+     * @covers \LBausch\CephRadosgwAdmin\Client
+     * @covers \LBausch\CephRadosgwAdmin\Config
+     * @covers \LBausch\CephRadosgwAdmin\Middlewares\SignatureMiddleware
+     * @covers \LBausch\CephRadosgwAdmin\Resources\AbstractResource
+     * @covers \LBausch\CephRadosgwAdmin\Resources\Bucket::policy
+     * @covers \LBausch\CephRadosgwAdmin\Signature\SignatureV4::signRequest
+     */
+    public function testBucketPolicyIsRetrieved()
+    {
+        $transactions = [];
+
+        $config = $this->getConfigWithMockedHandlers($transactions, [
+            new Response(200, [], <<<'EOT'
+{
+    "acl": {},
+    "owner": {
+        "id": "foo",
+        "display_name": "bar"
+    }
+}
+EOT),
+        ]);
+
+        $client = Client::make('http://gateway', 'acesskey', 'secretkey', $config);
+
+        $response = $client->bucket()->policy('mybucket');
+
+        /** @var Request $request */
+        $request = $transactions[0]['request'];
+
+        $this->assertEquals('GET', $request->getMethod());
+        $this->assertEquals('http://gateway/admin/bucket?policy=&bucket=mybucket', $request->getUri());
+
+        $this->assertEquals([
+            'acl' => [],
+            'owner' => [
+                'id' => 'foo',
+                'display_name' => 'bar',
+            ],
+        ], $response->get());
     }
 }
