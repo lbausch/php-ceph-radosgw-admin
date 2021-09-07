ce047d1c3fd1a793151b3e59cf0b85fc8fea3c20
Lorenz Bausch <info@lorenzbausch.de>
2021-09-07 09:24:35 +0200

    Add usage trim test

--- tests/Resources/UsageTest.php
+++ tests/Resources/UsageTest.php
@@ -42,4 +42,40 @@ final class UsageTest extends TestCase
         $this->assertEquals('gateway', $request->getUri()->getHost());
         $this->assertEquals('/admin/usage', $request->getUri()->getPath());
     }
+
+    /**
+     * @covers \LBausch\CephRadosgwAdmin\ApiRequest
+     * @covers \LBausch\CephRadosgwAdmin\ApiResponse
+     * @covers \LBausch\CephRadosgwAdmin\Client
+     * @covers \LBausch\CephRadosgwAdmin\Config
+     * @covers \LBausch\CephRadosgwAdmin\Middlewares\SignatureMiddleware
+     * @covers \LBausch\CephRadosgwAdmin\Resources\AbstractResource
+     * @covers \LBausch\CephRadosgwAdmin\Resources\Usage::trim
+     * @covers \LBausch\CephRadosgwAdmin\Signature\SignatureV4::signRequest
+     */
+    public function testUsageIsTrimmed(): void
+    {
+        $transactions = [];
+
+        $config = $this->getConfigWithMockedHandlers($transactions, [
+            new Response(200, [], '[]'),
+        ]);
+
+        $client = Client::make('http://gateway', 'acesskey', 'secretkey', $config);
+
+        $response = $client->usage()->trim([
+            'uid' => 'foo',
+            'start' => '1970-01-01 13:37:00',
+        ]);
+
+        $this->assertEquals([], $response->get());
+
+        $this->assertCount(1, $transactions);
+
+        /** @var Request $request */
+        $request = $transactions[0]['request'];
+
+        $this->assertEquals('DELETE', $request->getMethod());
+        $this->assertEquals('http://gateway/admin/usage?uid=foo&start=1970-01-01%2013%3A37%3A00', $request->getUri());
+    }
 }
