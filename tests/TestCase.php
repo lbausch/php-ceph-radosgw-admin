835177cd9efc892a1606bb674c0db7441b4b92d4
Lorenz Bausch <info@lorenzbausch.de>
2021-09-08 09:34:34 +0200

    Tweak docblocks

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -24,7 +24,7 @@ abstract class TestCase extends BaseTestCase
     /**
      * Get a handler stack with history middleware.
      *
-     * https://docs.guzzlephp.org/en/stable/testing.html#history-middleware
+     * @see https://docs.guzzlephp.org/en/stable/testing.html#history-middleware
      */
     protected function getHandlerStack(array &$container): HandlerStack
     {
@@ -40,7 +40,7 @@ abstract class TestCase extends BaseTestCase
     /**
      * Get a mock handler.
      *
-     * https://docs.guzzlephp.org/en/stable/testing.html#mock-handler
+     * @see https://docs.guzzlephp.org/en/stable/testing.html#mock-handler
      */
     protected function getMockHandler(array $responses = []): MockHandler
     {
